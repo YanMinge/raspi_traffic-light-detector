0fb5f2806c9f5b404e973136492a9f261e6ef8af
Yanminge <myan@makeblock.com>
2018-08-27 15:22:18 +0800

    add function time conut

--- test_script/take_photo.py
+++ test_script/take_photo.py
@@ -16,33 +16,33 @@ import time
 mouse_clicked = False
 
 def main(argv=None):
-	global mouse_clicked
-	cv2.namedWindow('raw_image')
+    global mouse_clicked
+    cv2.namedWindow('raw_image')
 
-	#cv2.setMouseCallback('raw_image',onMouse)
-	print 'Showing camera feed. click window or press exit key to stop.'
-	cameraCapture = cv2.VideoCapture(0)
-	success, frame = cameraCapture.read()
+    #cv2.setMouseCallback('raw_image',onMouse)
+    print 'Showing camera feed. click window or press exit key to stop.'
+    cameraCapture = cv2.VideoCapture(0)
+    success, frame = cameraCapture.read()
 
-	while success and not mouse_clicked:
-		success,frame = cameraCapture.read()
-		raw_image = frame
-		result_image = frame
-		cv2.imshow('raw_image', raw_image)
-		key = cv2.waitKey(10)
-		if key != -1:
-			print("key=")
-			print(key)
-		if(key == 27):
-			break;
-		if(key == 112):
-			time_str = time.strftime("%H-%M-%S", time.gmtime())
-			image_path = "./image/" + "image-"+ time_str + ".jpeg"
-			print(image_path)
-			cv2.imwrite(image_path, raw_image)
+    while success and not mouse_clicked:
+        success,frame = cameraCapture.read()
+        raw_image = frame
+        result_image = frame
+        cv2.imshow('raw_image', raw_image)
+        key = cv2.waitKey(10)
+        if key != -1:
+            print("key=")
+            print(key)
+        if(key == 27):
+            break;
+        if(key == 112):
+            time_str = time.strftime("%H-%M-%S", time.gmtime())
+            image_path = "./image/" + "image-"+ time_str + ".jpeg"
+            print(image_path)
+            cv2.imwrite(image_path, raw_image)
 
-	cv2.destroyWindow('raw_image')
-	cameraCapture.release()
+    cv2.destroyWindow('raw_image')
+    cameraCapture.release()
 
 def onMouse(event,x,y,flags,param):
     global mouse_clicked
@@ -50,4 +50,4 @@ def onMouse(event,x,y,flags,param):
         mouse_clicked = True
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
