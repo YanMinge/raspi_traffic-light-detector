68dcd83ecd0829463e62a785eb2679202f472585
Yan minge <myan@makeblock.com>
2018-08-29 13:12:20 +0800

    update 201808291312

--- src/traffic_light_detector.py
+++ src/traffic_light_detector.py
@@ -11,6 +11,12 @@ import getopt
 import numpy as np
 import threading
 import time
+import serial
+import serial.tools.list_ports
+import re
+from threading import Timer
+import random
+import codecs, binascii
 
 # 全局变量定义
 TRAFFIC_LIGHT_STATE_NULL = 0
@@ -49,8 +55,42 @@ def main(argv=None):
 
 def uartDataProcess_task():
     global traffic_light_state
-    while( True ):
-        pass
+	 while True:
+    port_list = list(serial.tools.list_ports.comports())
+    if len(port_list) <= 0:
+        print "None port find!!"
+    else:
+        for i in range(len(port_list)):
+            port_list_0 = list(port_list[i])
+            port_serial = port_list_0[0]
+            #print port_serial
+            if re.match('/dev/ttyUSB',port_serial) != None:
+                try:
+                    ser = serial.Serial(port_serial, 115200, timeout = 0.5)
+                    if(ser is not None):
+                        data_frame = ser.readline()
+                        if data_frame != "":
+                            print data_frame[0]
+                            if (data_frame[0] == 'g') or (data_frame[0] == 'G'):
+                                cmd_data =  data_frame.split()
+                                if int(cmd_data[0][1:]) == 11:
+                                    print "set_color_ranger"
+                                    for i in range(1,len(cmd_data)):
+                                        data_str = cmd_data[i][1:]
+                                        print int(data_str)
+                                elif int(cmd_data[0][1:]) == 12:
+                                    print "set_hough_circles_para"
+                                elif int(cmd_data[0][1:]) == 13:
+                                    print "set_hs_threshold"
+                        ser.write("G20 L1\r\n")
+                        sleep(0.01)
+                        ser.write("G20 L0\r\n")
+                        sleep(0.01)
+                        #ser.close()
+                except Exception, error:
+                    print("Exception:" + str(Exception))
+                    print("error:" + str(error))
+    sleep(0.5)
 
 def timeCount_task():
     global timeCount
--- test_script/TurnToTestScript.py
+++ test_script/TurnToTestScript.py
@@ -13,20 +13,36 @@ import codecs, binascii
 while True:
     port_list = list(serial.tools.list_ports.comports())
     if len(port_list) <= 0:
-	print "None port find!!"
+        print "None port find!!"
     else:
         for i in range(len(port_list)):
-	    port_list_0 = list(port_list[i])
+            port_list_0 = list(port_list[i])
             port_serial = port_list_0[0]
-	    print port_serial
+            #print port_serial
             if re.match('/dev/ttyUSB',port_serial) != None:
                 try:
-	            ser = serial.Serial(port_serial, 115200, timeout = 0.5)
+                    ser = serial.Serial(port_serial, 115200, timeout = 0.5)
                     if(ser is not None):
-                        print (' ... OK')
-                        ser.write("\xF3\xF6\x03\x00\x0A\x00\x03\x0D\xF4")
-                        sleep(0.2)
-                        ser.close()
-                except:
-                    print("Write Error!!!")
+                        data_frame = ser.readline()
+                        if data_frame != "":
+                            print data_frame[0]
+                            if (data_frame[0] == 'g') or (data_frame[0] == 'G'):
+                                cmd_data =  data_frame.split()
+                                if int(cmd_data[0][1:]) == 11:
+                                    print "set_color_ranger"
+                                    for i in range(1,len(cmd_data)):
+                                        data_str = cmd_data[i][1:]
+                                        print int(data_str)
+                                elif int(cmd_data[0][1:]) == 12:
+                                    print "set_hough_circles_para"
+                                elif int(cmd_data[0][1:]) == 13:
+                                    print "set_hs_threshold"
+                        ser.write("G20 L1\r\n")
+                        sleep(0.01)
+                        ser.write("G20 L0\r\n")
+                        sleep(0.01)
+                        #ser.close()
+                except Exception, error:
+                    print("Exception:" + str(Exception))
+                    print("error:" + str(error))
     sleep(0.5)
